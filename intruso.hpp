77f83480f2b88c909b43f151f2a17109df261858
Bruno Lima Soares <112128273+BrunoL28@users.noreply.github.com>
2022-11-01 20:04:37 -0300

    Atualização do arquivo Intruso.hpp
    No arquivo hpp implementamos os membros privados da classe Intruso, um vector de string que armazenará números e um vector de string que armazenará letras. A escolha pelo vector em detrimento do map ou multimap veio a partir da percepção de que poderiamos navegar o vector acessando as strings por meio de índice, favorecendo o armazenamento e manipulação dos dados das senhas.

--- intruso.hpp
+++ intruso.hpp
@@ -1,9 +1,15 @@
 #include <string>
+#include <vector>
 
 class Intruso{
     /*Continue a implementação da classe Intruso*/
+    private:
+
+        std::vector<std::string> _numbers;
+        std::vector<std::string> _letters;
 
     public:
+
         void set_senha_vazada(std::string vazou);
         std::string crack_senha();
 };
